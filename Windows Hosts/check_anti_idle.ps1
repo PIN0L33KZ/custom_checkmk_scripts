17d477854e0dc38ac60030ee1bcce3dbd5898afb
PIN0L33KZ <76883164+PIN0L33KZ@users.noreply.github.com>
2024-08-21 01:43:33 +0200

    Update check_anti_idle.ps1

--- Windows Hosts/check_anti_idle.ps1
+++ Windows Hosts/check_anti_idle.ps1
@@ -1,1 +1,8 @@
-$processName = "ANTI_IDLE_SERVICE_NAME" #Change to your anti idle handler.$process = Get-Process -Name $processName -ErrorAction SilentlyContinueif ($process) {    Write-Output "0 ""Anti Idle"" - ANTI_IDLE_SERVICE_NAME is running" #Change to your anti idle handler.} else {    Write-Output "2 ""Anti Idle"" - ANTI_IDLE_SERVICE_NAME is not running" #Change to your anti idle handler.}
+$processName = "ANTI_IDLE_SERVICE_NAME" #Change to your anti idle handler.
+$process = Get-Process -Name $processName -ErrorAction SilentlyContinue
+
+if ($process) {
+    Write-Output "0 ""Anti Idle"" - ANTI_IDLE_SERVICE_NAME is running" #Change to your anti idle handler.
+} else {
+    Write-Output "2 ""Anti Idle"" - ANTI_IDLE_SERVICE_NAME is not running" #Change to your anti idle handler.
+}
