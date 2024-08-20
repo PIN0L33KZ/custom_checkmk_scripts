f13c78c17e8e28477739809fde0fdfddd927bab8
PIN0L33KZ <76883164+PIN0L33KZ@users.noreply.github.com>
2024-08-21 01:43:55 +0200

    Update check_discord.ps1

--- Windows Hosts/check_discord.ps1
+++ Windows Hosts/check_discord.ps1
@@ -1,1 +1,9 @@
-$processName = "Discord" #Check if your Instance's name is Discord indeed.$process = Get-Process -Name $processName -ErrorAction SilentlyContinueif ($process) {    Write-Output "0 ""Discord"" - Instance is running"} else {    Write-Output "2 ""Discord"" - Instance is not running"}
+$processName = "Discord" #Check if your Instance's name is Discord indeed.
+
+$process = Get-Process -Name $processName -ErrorAction SilentlyContinue
+
+if ($process) {
+    Write-Output "0 ""Discord"" - Instance is running"
+} else {
+    Write-Output "2 ""Discord"" - Instance is not running"
+}
